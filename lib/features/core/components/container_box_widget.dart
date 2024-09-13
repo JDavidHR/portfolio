643d21a9957e656ced2c0f3f01e10caf968fb802
JDavidHR <simond202020@gmail.com>
2024-09-13 16:28:21 -0500

    EDIT - pressed button

--- lib/features/core/components/container_box_widget.dart
+++ lib/features/core/components/container_box_widget.dart
@@ -60,7 +60,8 @@ class _ContainerBoxWidgetState extends State<ContainerBoxWidget> {
                   ),
                 ),
                 IconButton(
-                  onPressed: widget.onPressed,
+                  onPressed: null,
+                  //onPressed: widget.onPressed,
                   icon: Icon(widget.icon ?? Icons.link_rounded),
                 ),
               ],
